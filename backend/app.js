c346d70ff1142d38bad0a478909542333cff97db
OSCAR DARIO RENDON ZULUAGA <75097205+OSCARstacku@users.noreply.github.com>
2023-02-06 17:51:32 -0500

    First commit

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const express=require('express');
+const bodyparser=require('body-parser');
+const mongoose=require('mongoose');
+const app=express();
+
+let port=process.env.PORT || 4299;
+
+// Para socket.io
+const server=require('http').createServer(app);
+const io=require('socket.io')(server,{
+    cors: {origin: '*'}
+});
+
+// Verificación conexión a DB
+mongoose.connect('mongodb://localhost:27017/test',(err,res)=>{
+    if(err){
+        //console.log(err);
+    }else{
+        // app.listen.. se cambia por server.listen en socket.io
+        server.listen(port,function(){
+            // console.log('Servidor corriendo en el puerto '+port);
+        })
+    }
+})
+
+// Rutas
+
+
+// Parsear (analizar cuerpos o estructuras), configurar DATA
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json({limit:'50mb',extended:true}));
+
+// Sincronizar conexión backend con Frontend en diferentes Droplets
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Allow','GET, PUT, POST, DELETE, OPTIONS');
+    next();
+});
+
+// Definición de APIs(rutas) a usar
+
+
+module.exports=app;
