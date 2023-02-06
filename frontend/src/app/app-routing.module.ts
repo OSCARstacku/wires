1792f97542489df467c9d7afd18597e6eb68bff6
OSCAR DARIO RENDON ZULUAGA <75097205+OSCARstacku@users.noreply.github.com>
2023-02-06 17:54:09 -0500

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignComponent } from './components/sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-in/up',component:SignComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
